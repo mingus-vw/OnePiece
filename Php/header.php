d4bba9606f4ea4ad5d1dfdd11d140b4b2da184e4
FIRST_NAME LAST_NAME <165249@student.horizoncollege.nl>
2024-12-05 10:56:57 +0100

    small navbar fixed

--- Php/header.php
+++ Php/header.php
@@ -12,7 +12,7 @@ function getEmail() {
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="../Css/header.css">
-<nav class="navbar navbar-expand-lg navbar-light sticky-top" id="navbar-base">
+<nav class="navbar-base navbar-expand-lg navbar-light sticky-top" id="navbar-base">
     <a href="index.php">
     <img src="../Images/One_Piece_Logo-min.png" alt="Logo" id="navbar-logo">
     </a>
@@ -26,16 +26,32 @@ function getEmail() {
                 <a class="nav-link" href="characters.php">Characters</a>
             </li>
             <li class="nav-item active" id="navbar-text">
-                <a class="nav-link" href="marines.php">Marines</a>
-            </li>
-            <li class="nav-item active" id="navbar-text">
                 <a class="nav-link" href="crews.php">Crews</a>
             </li>
             <li class="nav-item active" id="navbar-text">
-                <a class="nav-link" href="devilfruits.php">Devil fruits</a>
+                <a class="nav-link" href="marines.php">Devil Fruits</a>
             </li>
         </ul>
     </div>
+    <div class="dropdown">
+  <button
+    class="btn btn-secondary dropdown-toggle"
+    type="button"
+    id="dropdownMenuButton"
+    data-toggle="dropdown"
+    aria-haspopup="true"
+    aria-expanded="false">
+    Dropdown button
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="islands.php">Islands</a>
+    <a class="dropdown-item" href="marines.php">Marines</a>
+    <a class="dropdown-item" href="pirates.php">Pirates</a>
+    <a class="dropdown-item" href="#">Top 100 PR</a>
+  </div>
+</div>
+
+
 
     <div class="sign-in-button">
         <?php if (isLoggedIn()): ?>
@@ -98,4 +114,7 @@ function getEmail() {
         </div>
     </div>
     <script src="../Js/login.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </nav>
